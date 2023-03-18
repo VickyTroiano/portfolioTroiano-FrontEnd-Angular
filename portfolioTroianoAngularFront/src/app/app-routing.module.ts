a143f37c907e43c8cf4125526013694e6a6f7bca
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-03-18 00:02:13 -0300

    Creación de los componentes login, el panel y navbar para dicho panel
    Tambien, se agregaron dos botones distintos para cuando nos encontramos en el panel (button-logout) y fuera de este mismo (button-login). En el app-routing.module.ts se agregaron las rutas login, index y panel y tambien se improtaron los componentes.

--- portfolioTroianoAngularFront/src/app/app-routing.module.ts
+++ portfolioTroianoAngularFront/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { PanelComponent } from './panel/panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'index', component:IndexComponent},
+  {path: 'panel', component:PanelComponent},
+  {path: '', redirectTo: '/index', pathMatch: 'full'}
+  
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolioTroianoAngularFront/src/app/app.module.ts
+++ portfolioTroianoAngularFront/src/app/app.module.ts
@@ -15,6 +15,12 @@ import { EducationComponent } from './education/education.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { PanelComponent } from './panel/panel.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarPanelComponent } from './navbar-panel/navbar-panel.component';
+import { ButtonLoginComponent } from './button-login/button-login.component';
+import { LoginComponent } from './login/login.component';
+import { ButtonLogoutComponent } from './button-logout/button-logout.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +36,13 @@ import { FooterComponent } from './footer/footer.component';
     EducationComponent,
     ProjectsComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    PanelComponent,
+    IndexComponent,
+    NavbarPanelComponent,
+    ButtonLoginComponent,
+    LoginComponent,
+    ButtonLogoutComponent
   ],
   imports: [
     BrowserModule,
