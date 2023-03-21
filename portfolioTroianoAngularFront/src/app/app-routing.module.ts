3a2496d145e326c0b3f141dd1a1fffe6ce63b129
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-03-21 18:36:37 -0300

    Corrección del uso del <br> por ul para alinear verticalmente los items

--- portfolioTroianoAngularFront/src/app/app-routing.module.ts
+++ portfolioTroianoAngularFront/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { PanelComponent } from './panelComponents/panel/panel.component';
