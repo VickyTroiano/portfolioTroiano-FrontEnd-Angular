83c21cedeaa2aa7726df5e1dbbed34be7752ea9c
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-03-27 11:36:36 -0300

    Creación de directivas dentro del componente projects

--- portfolioTroianoAngularFront/src/app/sections/projects/projects.component.ts
+++ portfolioTroianoAngularFront/src/app/sections/projects/projects.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/services/datos.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  infoProjects: any=[];
+  
+  constructor(private datosService:DatosService){}
+	
+	ngOnInit(): void {
+     this.datosService.getDatos().subscribe(datos=> {
+          console.log(datos)
+          // this.bannerImg = datos.profile.bannerImg;
+          this.infoProjects = datos.projects;
+          
+        });
+	    }
 
 }
