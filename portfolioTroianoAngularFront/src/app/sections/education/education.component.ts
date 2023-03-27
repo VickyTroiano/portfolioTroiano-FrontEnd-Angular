83861d849f10787e1f3cee98ab73baf5852809b3
Victoria Troiano <92123389+VickyTroiano@users.noreply.github.com>
2023-03-27 10:43:45 -0300

    Creacion de directivas dentro del componente education

--- portfolioTroianoAngularFront/src/app/sections/education/education.component.ts
+++ portfolioTroianoAngularFront/src/app/sections/education/education.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/services/datos.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
+  formalEducation: any=[];
+  informalEducation: any=[];
+ 
+  constructor(private datosService:DatosService){}
+	
+	ngOnInit(): void {
+     this.datosService.getDatos().subscribe(datos=> {
+          console.log(datos)
+          // this.bannerImg = datos.profile.bannerImg;
+          this.formalEducation = datos.education_formal;
+          this.informalEducation = datos.education_informal;
+        });
+	    }
+	}
 
-}
